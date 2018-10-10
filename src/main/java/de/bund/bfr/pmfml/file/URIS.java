83cdff8bdf453b720de721ab46ed36b4430e614a
miguelalba <miguel.de-alba-aparicio@bfr.bund.de>
2018-10-10 18:40:26 +0200

    Change access of URIS to protected

--- src/main/java/de/bund/bfr/pmfml/file/URIS.java
+++ src/main/java/de/bund/bfr/pmfml/file/URIS.java
@@ -1,7 +1,6 @@
 package de.bund.bfr.pmfml.file;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * Identifiers of a COMBINE specification.
@@ -10,18 +9,18 @@ import java.net.URISyntaxException;
  * @see <a href="http://co.mbine.org/standards/specifications/">http://co.mbine.
  *      org/standards/specifications/</a>
  */
-public class URIS {
+class URIS {
 
     /**
      * COMBINE has no official support for NuML, so it has no identifier for NuML.
      * NuML's schema is used instead.
      */
-    public static final URI numl = URI.create("https://raw.githubusercontent.com/NuML/NuML/master/NUMLSchema.xsd");
+    static final URI numl = URI.create("https://raw.githubusercontent.com/NuML/NuML/master/NUMLSchema.xsd");
 
     /** Generic SBML URI */
-    public static final URI sbml = URI.create("http://identifiers.org/combine/specifications/sbml");
+    static final URI sbml = URI.create("http://identifiers.org/combine/specifications/sbml");
 
-    public static final URI pmf = URI.create("http://sourceforge.net/projects/microbialmodelingexchange/files/");
+    static final URI pmf = URI.create("http://sourceforge.net/projects/microbialmodelingexchange/files/");
 
-    public static final URI txt = URI.create("http://purl.org/NET/mediatypes/text-xplain");
+    static final URI txt = URI.create("http://purl.org/NET/mediatypes/text-xplain");
 }
